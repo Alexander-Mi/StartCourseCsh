2476a448d030a04565167aa577cc6da0d299c59d
Alexander-Mi <mishankinas@gmail.com>
2021-05-06 15:34:07 +0200

    тестовая отправка

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 
-namespace ex1
+namespace Ex1
 {
     class Program
     {
         static void Main(string[] args)
         {
-          // Мишанкин А
+            // Мишанкин А
+            // Написать метод, возвращающий минимальное из трёх чисел.
 
-          //  1.Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес).В результате вся информация выводится в одну строчку:
-          //  а) используя склеивание;
-          //  б) используя форматированный вывод;
-          //  в) используя вывод со знаком $.
+            Console.WriteLine("Введите а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            int c = Convert.ToInt32(Console.ReadLine());
 
+            int d = minimum(a, b, c);
 
-            // вводим данные
-            Console.WriteLine("Введите Ваше имя");
-            string name = Console.ReadLine();
+            Console.WriteLine($"Миниальное число: {d}");
 
-            Console.WriteLine("Введите Вашу фамилию");
-            string surname = Console.ReadLine();
-
-            Console.WriteLine("Введите Ваш рост, в метрах например 185,5");
-            double height = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите Ваш вес в кг, например 75,5");
-            double weight = Convert.ToDouble(Console.ReadLine());
-
-            // выводим данные разными способами (Задание 1)
-            Console.WriteLine("---------------");
-            Console.WriteLine(name + surname + height + weight);
-            Console.WriteLine(" Имя " + name + " Фамилия " + surname + " Рост " + height + " Вес " + weight);
-            Console.WriteLine($" Имя: { name}, Фамилия: { surname}, Рост: { height}, Вес: { weight}");
         }
+
+        private static int minimum(int a, int b, int c)
+        {
+            int d; // Минимальная переменная 
+
+            if (a < b && a < c) d = a;
+            else if (b < c) d = b;
+            else d = c;
+            return d;
+        }
+
     }
 }
+
