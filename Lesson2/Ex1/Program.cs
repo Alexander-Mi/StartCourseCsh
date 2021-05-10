eb36181994d0cb9c3b61de5099a87ec8d3c01169
Alexander-Mi <mishankinas@gmail.com>
2021-05-10 10:15:06 +0200

    второй урок 3 задания

---
+++ Lesson2/Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Мишанкин А
+            // Написать метод, возвращающий минимальное из трёх чисел.
+
+            Console.WriteLine("Введите а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int d = minimum(a, b, c);
+
+            Console.WriteLine($"Миниальное число: {d}");
+
+        }
+
+        private static int minimum(int a, int b, int c)
+        {
+            int d; // Минимальная переменная 
+
+            if (a < b && a < c) d = a;
+            else if (b < c) d = b;
+            else d = c;
+            return d;
+        }
+
+    }
+}
+
